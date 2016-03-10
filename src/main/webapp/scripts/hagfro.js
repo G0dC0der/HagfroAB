5cec547e5f6b9e330cb153d9226fb5e5945218e0
Pojahn Moradi <pmoradi@atex.com>
2016-03-10 16:28:48 +0100

    2016-03-10: All pages are done. Looking at improving the trademark

--- src/main/webapp/scripts/hagfro.js
+++ src/main/webapp/scripts/hagfro.js
@@ -1,1 +1,72 @@
+var Hagfro = null;
 
+$(function(){
+    Hagfro = {};
+
+    Hagfro.construct = function(params) {
+        var body = $('body');
+        var page = Hagfro.getPage();
+        var pageContent = body.html();
+
+        body.html(se.hagfro.renderSite({
+            title: params.title,
+            renderTrademark: params.renderTrademark,
+            pages: [
+                {
+                    'title': 'Hem',
+                    'url': 'home.html',
+                    'selected': page === 'home.html'
+                },
+                {
+                    'title': 'Hagfro',
+                    'url': 'hagfro.html',
+                    'selected': page === 'hagfro.html'
+                },
+                {
+                    'title': 'Familjehem',
+                    'url': 'familjehem.html',
+                    'selected': page === 'familjehem.html'
+                },
+                {
+                    'title': 'Intresseanmälan',
+                    'url': 'intresseanmalan.html',
+                    'selected': page === 'intresseanmalan.html'
+                },
+                {
+                    'title': 'Verksamhet',
+                    'url': 'verksamhet.html',
+                    'selected': page === 'verksamhet.html'
+                },
+                {
+                    'title': 'Kontakt',
+                    'url': 'kontakt.html',
+                    'selected': page === 'kontakt.html'
+                }
+            ]
+        }).content);
+
+        $('.page-content').append(pageContent);
+
+        $('.footer-text').on('click', function(){
+            alert('Site created by Pojahn Moradi, HagFro AB collaborator.');
+        });
+    };
+
+    Hagfro.getPage = function() {
+        var url = window.location.href.split('?')[0];
+        return url.substring(url.lastIndexOf('/') + 1);
+    };
+
+    Hagfro.constructMap = function(id) {
+        var position = new google.maps.LatLng(59.42132,	17.84826);
+        var map = new google.maps.Map(document.getElementById(id), {
+            center: position,
+            zoom: 15
+        });
+
+        var marker=new google.maps.Marker({
+          position:position,
+        });
+        marker.setMap(map);
+    };
+});
--- src/main/webapp/scripts/soy/pagestuff.soy.js
+++ src/main/webapp/scripts/soy/pagestuff.soy.js
@@ -10,9 +10,33 @@ if (typeof se == 'undefined') { var se = {}; }
 if (typeof se.hagfro == 'undefined') { se.hagfro = {}; }
 
 
-se.hagfro.test = function(opt_data, opt_ignored) {
-  return soydata.VERY_UNSAFE.ordainSanitizedHtml('<div>CRAP! ' + soy.$$escapeHtml(opt_data.TEST) + '</div>');
+se.hagfro.renderSite = function(opt_data, opt_ignored) {
+  return soydata.VERY_UNSAFE.ordainSanitizedHtml('<div class="page">' + se.hagfro.renderHeader(opt_data) + ((opt_data.renderTrademark) ? se.hagfro.trademarks(null) : '') + '<div class="page-content"><div class="page-title">' + soy.$$escapeHtml(opt_data.title) + '</div><div class="short-line"></div></div><div class="clearer" style="padding-bottom:50px;"></div><div class="bottom-sapce"></div><div class="footer"><span class="footer-text">\u00A9 HagFro AB 2015</span></div></div>');
 };
 if (goog.DEBUG) {
-  se.hagfro.test.soyTemplateName = 'se.hagfro.test';
+  se.hagfro.renderSite.soyTemplateName = 'se.hagfro.renderSite';
+}
+
+
+se.hagfro.renderHeader = function(opt_data, opt_ignored) {
+  var output = '<div class="menu"><ul>';
+  var pageList25 = opt_data.pages;
+  var pageListLen25 = pageList25.length;
+  for (var pageIndex25 = 0; pageIndex25 < pageListLen25; pageIndex25++) {
+    var pageData25 = pageList25[pageIndex25];
+    output += (pageData25.selected) ? '<li class="tab-selected"><span class="fake-a">' + soy.$$escapeHtml(pageData25.title) + '</span></li>' : '<li><a href="' + soy.$$escapeHtmlAttribute(soy.$$filterNormalizeUri(pageData25.url)) + '">' + soy.$$escapeHtml(pageData25.title) + '</a></li>';
+  }
+  output += '</ul></div><hr class="separator"/>';
+  return soydata.VERY_UNSAFE.ordainSanitizedHtml(output);
+};
+if (goog.DEBUG) {
+  se.hagfro.renderHeader.soyTemplateName = 'se.hagfro.renderHeader';
+}
+
+
+se.hagfro.trademarks = function(opt_data, opt_ignored) {
+  return soydata.VERY_UNSAFE.ordainSanitizedHtml('<div class="welcome-dialog"><div class="welcome-dialog-welcome">V\u00E4lkommen till</div><div class="welcome-dialog-welcome welcome-dialog-hagfro">HagFro AB</div><div class="welcome-dialog-desc">Kunskap f\u00F6r integration</div></div><div class="trademark"><div class="trademark-left">HF</div><div class="trademark-middle"></div><div class="trademark-right">HagFro AB</div></div><div class="clearer" style="padding-bottom:50px;"></div>');
+};
+if (goog.DEBUG) {
+  se.hagfro.trademarks.soyTemplateName = 'se.hagfro.trademarks';
 }
